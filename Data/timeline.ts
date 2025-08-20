6b2f173d136695080e23eba151282d9d78a75687
flagrantii <tanuson679@gmail.com>
2025-08-20 20:05:23 +0700

    update timeline data

--- Data/timeline.ts
+++ Data/timeline.ts
@@ -1,68 +1,66 @@
 export type TimelineItem = {
-    id: number
-    company: string
-    role: string
-    type: string
-    period: string
-    bullets: string[]
-  }
-  
-  export const timelineItems: TimelineItem[] = [
-    {
-      id: 1,
-      company: 'LINE MAN Wongnai',
-      role: 'Site Reliability Engineer',
-      type: 'Part-Time',
-      period: 'Aug 2025 – Now',
-      bullets: [
-        'Platform team'
-      ],
-    },
-    {
-      id: 2,
-      company: 'LINE MAN Wongnai',
-      role: 'Software Engineer',
-      type: 'Internship',
-      period: 'May 2025 – July 2025',
-      bullets: [
-        'Designed and implemented scalable backend services in Go handling 200k+ RPS and >1M DB ops.',
-        'Implemented fault‑tolerant services to ensure seamless UX during traffic surges.',
-      ],
-    },
-    {
-      id: 3,
-      company: 'Swipe',
-      role: 'Full‑Stack Developer',
-      type: 'Part‑Time',
-      period: 'Apr 2024 – Nov 2024',
-      bullets: [
-        'Developed responsive PWA from detailed Figma designs using Next.js, TypeScript, MUI, Redux.',
-        'Enhanced performance through efficient coding practices and tuning by best‑practice solutions.',
-        'Built and shipped APIs for the main product to extend capabilities and performance.',
-      ],
-    },
-    {
-      id: 4,
-      company: 'Blockfint',
-      role: 'Software Developer',
-      type: 'Internship',
-      period: 'May 2024 – Jul 2024',
-      bullets: [
-        'Implemented concurrency techniques to boost API speed and strengthen security.',
-        'Built a user‑friendly carbon credit offset calculator in Next.js and TypeScript.',
-        'Improved UX for core-product modules to solve critical challenges with innovative solutions.',
-      ],
-    },
-    {
-      id: 5,
-      company: 'Freelance',
-      role: 'Software Engineer / Project Manager',
-      type: 'Since 2022',
-      period: 'Since 2022',
-      bullets: [
-        'Built bespoke websites for businesses and startups; ensured consistent branding and UX.',
-        'Developed e‑commerce solutions with secure payments; custom dashboards in Next.js.',
-        'Implemented inventory and order systems handling 5,000+ monthly transactions.',
-      ],
-    },
-  ]
+  id: number;
+  company: string;
+  role: string;
+  type: string;
+  period: string;
+  bullets: string[];
+};
+
+export const timelineItems: TimelineItem[] = [
+  {
+    id: 1,
+    company: "LINE MAN Wongnai",
+    role: "Software Engineer, platform",
+    type: "Part-Time",
+    period: "Aug 2025 – Now",
+    bullets: ["Developed Observability system"],
+  },
+  {
+    id: 2,
+    company: "LINE MAN Wongnai",
+    role: "Software Engineer, backend",
+    type: "Internship",
+    period: "May 2025 – July 2025",
+    bullets: [
+      "Designed and implemented scalable backend services in Go handling 200k+ RPS and >1M DB ops.",
+      "Implemented fault‑tolerant services to ensure seamless UX during traffic surges.",
+    ],
+  },
+  {
+    id: 3,
+    company: "Swipe",
+    role: "Full‑Stack Developer",
+    type: "Part‑Time",
+    period: "Apr 2024 – Nov 2024",
+    bullets: [
+      "Developed responsive PWA from detailed Figma designs using Next.js, TypeScript, MUI, Redux.",
+      "Enhanced performance through efficient coding practices and tuning by best‑practice solutions.",
+      "Built and shipped APIs for the main product to extend capabilities and performance.",
+    ],
+  },
+  {
+    id: 4,
+    company: "Blockfint",
+    role: "Software Developer",
+    type: "Internship",
+    period: "May 2024 – Jul 2024",
+    bullets: [
+      "Implemented concurrency techniques to boost API speed and strengthen security.",
+      "Built a user‑friendly carbon credit offset calculator in Next.js and TypeScript.",
+      "Improved UX for core-product modules to solve critical challenges with innovative solutions.",
+    ],
+  },
+  {
+    id: 5,
+    company: "Freelance",
+    role: "Software Engineer / Project Manager",
+    type: "Since 2022",
+    period: "Since 2022",
+    bullets: [
+      "Built bespoke websites for businesses and startups; ensured consistent branding and UX.",
+      "Developed e‑commerce solutions with secure payments; custom dashboards in Next.js.",
+      "Implemented inventory and order systems handling 5,000+ monthly transactions.",
+    ],
+  },
+];
